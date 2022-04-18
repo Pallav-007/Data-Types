f8de898c898d80aa786cabefa46b796a46dab707
Pallav-007 <103948779+Pallav-007@users.noreply.github.com>
2022-04-18 21:31:42 +0545

    Add files via upload

---
+++ 02Data Type.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+/*
+Data Types in c
+int:1,2,3,-100,5+5.5=10
+float:13.5+5=18.5
+char:A-Z,a-z,symbols[@#$%]
+*/
+	int main(){
+	
+	  int a=10;
+	  float b=13.3;
+	  char c='H';
+	  
+	  printf("%d\n",a);
+	  printf("%.2f\n",b);
+	  printf("%c\n",c);
+	
+	return 0;
+}
